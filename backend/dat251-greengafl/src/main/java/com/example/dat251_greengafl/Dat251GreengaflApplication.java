8dc46ed26cb5d8d83f7103774b74632e4ec07800
SigvardHelsingen <sihel6491@hvl.no>
2026-02-06 11:01:19 +0100

    initial backend commit

---
+++ backend/dat251-greengafl/src/main/java/com/example/dat251_greengafl/Dat251GreengaflApplication.java
@@ -0,0 +1,13 @@
+package com.example.dat251_greengafl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat251GreengaflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dat251GreengaflApplication.class, args);
+	}
+
+}
---
+++ backend/dat251-greengafl/src/test/java/com/example/dat251_greengafl/Dat251GreengaflApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dat251_greengafl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat251GreengaflApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
