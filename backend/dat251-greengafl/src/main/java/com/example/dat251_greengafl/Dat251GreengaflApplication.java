77a382587dcc201dfbe1ac2af95ed49356130803
Hanna Rasmussen <haha2701@gmail.com>
2026-02-26 13:12:30 +0100

    testing pipeline

--- backend/dat251-greengafl/src/main/java/com/example/dat251_greengafl/Dat251GreengaflApplication.java
+++ backend/dat251-greengafl/src/main/java/com/example/dat251_greengafl/Dat251GreengaflApplication.java
@@ -3,6 +3,8 @@ package com.example.dat251_greengafl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// hei jeg er en test
+
 @SpringBootApplication
 public class Dat251GreengaflApplication {
 
